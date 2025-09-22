fd59e346ebb59ab6ac70366ebc70701c8beec082
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-22 21:48:25 +0200

    add: submit func tasks

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,43 @@
-import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
 import { useRouter } from "expo-router";
+import { useState } from "react";
+
+const initialList = [
+  { id: 1, name: "Coffee" },
+  { id: 2, name: "Tea" },
+  { id: 3, name: "Milk" },
+];
 
 export default function App() {
-  const router = useRouter();
+  const [value, setValue] = useState("");
+  const [shoppingList, setShoppingList] = useState(initialList);
+
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList = [
+        { id: new Date().toISOString(), name: value },
+        ...shoppingList,
+      ];
+      setShoppingList(newShoppingList);
+      setValue("");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <ShoppingListItem text={"Coffee"} />
-      <ShoppingListItem text={"Tea"} isCompleted />
-      <ShoppingListItem text={"Sugar"} isCompleted />
+      <TextInput
+        style={styles.textInput}
+        placeholder="E.g. Coffee"
+        value={value}
+        onChangeText={setValue}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+      {shoppingList.map((todo) => (
+        <ShoppingListItem text={todo.name} key={todo.id} />
+      ))}
     </View>
   );
 }
@@ -18,6 +46,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+    paddingTop: 12,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
   },
 });
