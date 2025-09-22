c8d87bb7c66e36d21a003ae61692f01bf8314e69
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-22 20:51:38 +0200

    add: created app folder

---
+++ app/index.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
+import { theme } from "../theme";
+import { ShoppingListItem } from "../components/ShoppingListItem";
+import { useRouter } from "expo-router";
+
+export default function App() {
+  const router = useRouter();
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => router.navigate("/idea")}>
+        <Text>Go to /idea</Text>
+      </TouchableOpacity>
+      <ShoppingListItem text={"Coffee"} />
+      <ShoppingListItem text={"Tea"} isCompleted />
+      <ShoppingListItem text={"Sugar"} isCompleted />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colorWhite,
+    justifyContent: "center",
+  },
+});
